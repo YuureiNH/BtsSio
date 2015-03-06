2bd4e3bdf23b5df9db3dde735d69656a64781f37
YuureiNH <hernandez.nicolas@outlook.fr>
2015-03-06 14:12:41 +0100

    Changement nom de classe

--- src/exercice_vendredi/FournisseurAcces.java
+++ src/exercice_vendredi/FournisseurAcces.java
@@ -9,14 +9,16 @@ package exercice_vendredi;
  *
  * @author Nicolas
  */
-public class Exercice_Vendredi {
-
+public class FournisseurAcces {
+    
+    
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         String mail[] = {"1@free.fr", "2@free.fr", "3@free.fr", "4@free.fr", "5@free.fr", "6@free.fr", "7@free.fr", "8@free.fr", "9@free.fr", "10@free.fr"}; 
-        
     }
     
 }
