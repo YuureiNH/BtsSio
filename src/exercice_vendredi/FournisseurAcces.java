2f55f6758ba7aa653d7b62a240400125730edb9f
YuureiNH <hernandez.nicolas@outlook.fr>
2015-03-06 16:25:44 +0100

    Affiche le nombre d'occurence en pourcentage

--- src/exercice_vendredi/FournisseurAcces.java
+++ src/exercice_vendredi/FournisseurAcces.java
@@ -13,12 +13,38 @@ public class FournisseurAcces {
     
     
     
+    private static void DetectionFournisseur(String tab[]) {
+        String[] fournisseur = new String[tab.length];
+        
+        for (int i = 0; i < tab.length; i++ ) {
+            int pos = tab[i].lastIndexOf("@");
+            fournisseur[i] = tab[i].substring(pos+1, tab[i].length());
+        }
+        FournisseurAcces.CompteurFournisseur(fournisseur);
+    }
     
+    public static void CompteurFournisseur(String tableau[]) {
+        String nom = null;
+        int[] fournisseur = new int[tableau.length];
+        int compteur = 0;
+        for (int i = 0; i < tableau.length; i++ ){
+            System.out.print("Nombre d'occurence de " + tableau [i] + " = "); 
+            compteur = 0;
+            for(int j = 0; j < tableau.length; j++){
+                if(tableau[i].equalsIgnoreCase(tableau [j]))
+                    compteur++;
+            }
+            System.out.println(compteur*10+"%");
+        }
+        System.out.println(fournisseur[0]);
+                
+    }
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String mail[] = {"1@free.fr", "2@free.fr", "3@free.fr", "4@free.fr", "5@free.fr", "6@free.fr", "7@free.fr", "8@free.fr", "9@free.fr", "10@free.fr"}; 
+        String mail[] = {"1@free.fr", "2@orange.fr", "3@free.fr", "4@free.fr", "5@free.fr", "6@free.fr", "7@free.fr", "8@free.fr", "9@free.fr", "10@free.fr"};
+        FournisseurAcces.DetectionFournisseur(mail);
     }
     
 }
